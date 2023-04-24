789655bf11258809031c0d51849e4e2782193bff
Monserrat Galván <55207425+mxgalvxn@users.noreply.github.com>
2023-04-24 16:17:28 -0600

    Update close_square.py

--- Challenge2/close_square.py
+++ Challenge2/close_square.py
@@ -11,7 +11,7 @@ from std_msgs.msg import Float32
 # Define robot parameters
 wheel_radius = 0.05  # radius of wheels (m)
 wheelbase = 0.2  # distance between wheels (m) (l)
-dt = 0.1  # time step (s)
+dt = 0.01  # time step (s)
 t = 0  # Total time (s)
 v_max = 1  # maximum linear velocity (m/s)
 w_max = np.pi / 2  # maximum angular velocity (rad/s)
@@ -22,16 +22,13 @@ y = 0  # y-position (m)
 theta = 0  # orientation (rad)
 
 # Define reference positions
-positions = np.array([[0, 0], [0, 2], [2, 2], [2, 0]])
+positions = np.array([[0, 0], [-1, -1], [1, -1], [1, 1]])
 num_positions = positions.shape[0]
 
 # Define gains for the PID controller
-kpr = 3
-kpt = 4
+kpr = 1
+kpt = 1
 
-# Initialize error and integral error for PID controller
-error_int = 0.0
-prev_error = 0.0
 
 wr = 0.0
 wl = 0.0
@@ -57,7 +54,7 @@ v_desired = 0.5  # linear velocity (m/s)
 w_desired = np.pi / 4  # angular velocity (rad/s)
 
 twist = Twist()
-vmax = 5
+vmax = 8
 
 if __name__=="__main__":
     try:
@@ -69,20 +66,11 @@ if __name__=="__main__":
         # Simulate robot motion
         i = 1
         while i < num_positions:
-            # Get current position and orientation
             xd = positions[i,0]
             yd = positions[i,1]
 
-            theta = (wr-wl)*t/wheelbase
-            vx = math.cos(theta)*wheel_radius*(wl + wr)/2
-            vy = math.sin(theta)*wheel_radius*(wl + wr)/2
-
-            x = x + vx*dt
-            y = y + vy*dt
-            
-            print('x = ', x, ' y = ', wr)
+            # Get current position and orientation
             thetad = math.atan2((yd-y), (xd-x))
-
             error = math.sqrt((xd-x)**2 + (yd-y)**2)
 
             thetae = (theta - thetad)
@@ -91,21 +79,35 @@ if __name__=="__main__":
             elif thetae < -math.pi:
                 thetae = thetae + 2*math.pi
 
+            w = -kpr * thetae
+            v = vmax*math.tanh(error*kpt/vmax);
 
-            error_int = error_int + error*dt
-            error_diff = (error - prev_error)/dt
-            prev_error = error
 
-            v = -kpt * error
-            w = -kpr*thetae - kpt*error - 0.01*error_int - 0.1*error_diff
+            
+            vr = v + w * math.cos(theta)*wheel_radius*(wl + wr)/2
+            vl = v - w *math.sin(theta)*wheel_radius*(wl + wr)/2
+
+            v = (vr+vl)/2
+            w = (vr-vl)/wheelbase
+
+            vx = v * math.cos(theta)
+            vy = v * math.sin(theta)
+
+            x = x + vx*dt
+            y = y + vy*dt
+            theta = theta + w * dt
+            
+            print('x actual = ', x, 'x deseada = ', xd)
+            print('error =', error)
+            
 
             # Compute wheel velocities from desired linear and angular velocities
             twist.linear.x = v
-            twist.angular.z = w
+            twist.angular.z = theta
             pub.publish(twist)
 
             # Check if the robot has reached the desired position
-            if abs(error) < 0.1 and abs(thetae) < 0.1:
+            if abs(error) < 0.0001:
                 i += 1
             t = t + dt
             rate.sleep()
